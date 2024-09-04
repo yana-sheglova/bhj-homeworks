8984ff9e2f1351597997e9621e6dc0cec0fc461b
Yana Sheglova <yana.shheglova.98@bk.ru>
2024-09-04 15:21:12 +0300

    Task 2.3.1 correct2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,29 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const tooltipTrigger = document.querySelector('.has-tooltip');
+	const tooltipTriggers = document.querySelectorAll('.has-tooltip');
 	const tooltip = document.querySelector('.tooltip');
 
-	if (!tooltipTrigger || !tooltip) return;
+	if (!tooltip) return;
 
 	const closeTooltip = () => {
 		tooltip.classList.remove('tooltip_active');
 	};
 
-	tooltipTrigger.addEventListener('click', (event) => {
-		event.preventDefault();
+	tooltipTriggers.forEach((tooltipTrigger) => {
+		tooltipTrigger.addEventListener('click', (event) => {
+			event.preventDefault();
 
-		const tooltipText = tooltipTrigger.getAttribute('title');
-		tooltip.textContent = tooltipText;
+			const tooltipText = tooltipTrigger.getAttribute('title');
+			if (tooltipText) {
+				tooltip.textContent = tooltipText;
 
-		const rect = tooltipTrigger.getBoundingClientRect();
-		tooltip.style.left = `${rect.left + window.scrollX}px`;
-		tooltip.style.top = `${rect.bottom + window.scrollY + 5}px`;
+				const rect = tooltipTrigger.getBoundingClientRect();
+		        tooltip.style.left = `${rect.left + window.scrollX}px`;
+		        tooltip.style.top = `${rect.bottom + window.scrollY + 5}px`;
 
-		tooltip.classList.toggle('tooltip_active');
+				tooltip.classList.toggle('tooltip_active');
+			}
+		});
 	});
 
 	document.addEventListener('click', (event) => {
-		if (tooltipTrigger && tooltip && !tooltipTrigger.contains(event.target) && !tooltip.contains(event.target)) {
+		const isTooltipTrigger = Array.from(tooltipTriggers).some(trigger => trigger.contains(event.target));
+	    if (!isTooltipTrigger && tooltip.classList.contains('tooltip_active') && !tooltip.contains(event.target)) {
 			closeTooltip();
-		};
+		}
 	});
 });
